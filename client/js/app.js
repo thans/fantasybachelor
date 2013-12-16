f3bdddc9b8e5950ab0b55f201d2fbdfbdba7c851
Mitchell Loeppky <mitchell@loeppky.com>
2013-12-16 01:11:52 -0800

    Created Modal (Woman Bio's) Window

--- client/js/app.js
+++ client/js/app.js
@@ -21,6 +21,16 @@ var utils = {
     }
 }
 
+$.fn.backgroundImage = function(backgroundImage) {
+    return this.each(function() {
+        if (backgroundImage) {
+            $(this).css('background-image', 'url("' + backgroundImage + '")');
+        } else {
+            $(this).css('background-image', '');
+        }
+    });
+};
+
 var ContestantData = function() {
     var contestants = [
         {
@@ -42,7 +52,64 @@ var ContestantData = function() {
                 },
                 {
                     key: 'Height',
-                    value: '5’7”'
+                    value: '5\'7\"'
+                },
+                {
+                    key: 'Tattoos',
+                    value: 'None'
+                },
+                {
+                    key: 'Favorite Actress',
+                    value: 'Kate Winslet'
+                },
+                {
+                    key: 'Top 3 Favorite Groups/Artists',
+                    value: 'The Beatles, Bob Dylan, Radiohead'
+                },
+                {
+                    key: 'Favorite Sports Team',
+                    value: 'LA Clippers'
+                }
+            ],
+            questions: [
+                {
+                    key: 'What is your favorite memory from your childhood?',
+                    value: 'The family trip we took to Lake Tahoe. I\'d never seen anything more gorgeous in my life, I learned how to play foosball, ate California Sourdough toast and laughed til my belly hurt with my family.'
+                },
+                {
+                    key: 'What is your favorite holiday and why?',
+                    value: 'It\'s close. Thanksgiving because it\'s such a cool day for everyone to just be thankful, give back and eat! And I also LOVE the Fourth of July because it\'s so romantic and I\'m grateful for our freedom!'
+                },
+                {
+                    key: 'What do you hope to get out of participating in this television show?',
+                    value: 'I hope to find love! And if He decides I\'m not the girl for him, I hope to gain friendships, peace through time away, a greater understanding of people and love and the beautiful world around me.'
+                },
+                {
+                    key: 'If you wanted to really impress a man, what would you do and why?',
+                    value: 'I would try to find out some secret wish or childhood dream, then figure out a way to make it come true. Something they\'ve always wanted or wanted to try. We all have them and it means a lot when the person you love is invested in those things.'
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: 'Alli',
+            codeName: 'alli',
+            stats: [
+                {
+                    key: 'Age',
+                    value: '31'
+                },
+                {
+                    key: 'Occupation',
+                    value: 'Message Therapist'
+                },
+                {
+                    key: 'Hometown',
+                    value: 'Apopka, FL'
+                },
+                {
+                    key: 'Height',
+                    value: '5\'7\"'
                 },
                 {
                     key: 'Tattoos',
@@ -138,8 +205,8 @@ var WeekData = function() {
             {
                 id: 1,
                 name: 'Week 1',
-                openTime: new Date(2013, 11, 1),
-                closeTime: new Date(2013, 11, 30),
+                openTime: new Date(2013, 10, 1),
+                closeTime: new Date(2013, 10, 30),
                 remainingWomen: [
                     {
                         id: 1,
@@ -152,12 +219,15 @@ var WeekData = function() {
             {
                 id: 2,
                 name: 'Week 2',
-                openTime: new Date(2013, 12, 1),
-                closeTime: new Date(2013, 12, 30),
+                openTime: new Date(2013, 11, 1),
+                closeTime: new Date(2013, 11, 30),
                 remainingWomen: [
                     {
                         id: 1,
                         multiplier: 2
+                    },
+                    {
+                        id: 2
                     }
                 ],
                 selectedWomen: [1],
@@ -166,8 +236,8 @@ var WeekData = function() {
             {
                 id: 3,
                 name: 'Week 3',
-                openTime: new Date(2014, 1, 1),
-                closeTime: new Date(2014, 1, 30),
+                openTime: new Date(2014, 0, 1),
+                closeTime: new Date(2014, 0, 30),
                 remainingWomen: [
                     {
                         id: 1,
@@ -276,21 +346,47 @@ var NavigationManager = function() {
                 $('#women').empty();
                 $.each(weekData.remainingWomen, function(i, v) {
                     var woman = contestantData.getContestantById(v.id);
-                    $('<li>')
+                    var isSelected = _.contains(weekData.selectedWomen, v.id);
+
+                    var mode = 'selectable';
+                    if (isSelected) {
+                        mode = 'rejectable';
+                    } else if (moment().isBefore(weekData.openTime) || moment().isAfter(weekData.closeTime)) {
+                        mode = 'selectionClosed';
+                    };
+                    var clickHandler = function(selected) {
+                        if (selected) {
+                            mode = 'rejectable';
+                        } else {
+                            mode = 'selectable';
+                        }
+                        $('#women li[data-id="' + woman.id + '"]')
+                        .toggleClass('selected', selected)
+                        .off('click')
+                        .click(function() {
+                            bioModal.setContestant(woman, mode, clickHandler).show();
+                        });
+                    };
+
+                    var button = $('<li>')
                         .attr('data-id', woman.id)
                         .css('background-image', 'url("' + utils.getSmallImage(woman) + '")')
+                        .toggleClass('selected', isSelected)
                         .text(woman.name)
-                        .appendTo('#women');
+                        .click(function() {
+                            bioModal.setContestant(woman, mode, clickHandler).show();
+                        }).appendTo('#women');
                 });
 
                 // Set selected women
                 $('.selectedWomenLayout').hide();
                 var layout = $('.selectedWomenLayout.select' + weekData.numberOfSelections);
-                layout.find('.woman').css('background-image', '');
+                layout.find('.woman').backgroundImage('');
                 $.each(weekData.selectedWomen, function(i, v) {
                     layout
                         .find('.woman' + (i + 1))
-                        .css('background-image', 'url("' + utils.getMediumImage(contestantData.getContestantById(v)) + '")');
+                        .backgroundImage(utils.getMediumImage(contestantData.getContestantById(v)));
+
                 })
                 layout.show();
             })
@@ -303,3 +399,63 @@ var NavigationManager = function() {
     };
 }
 var navManager = new NavigationManager();
+
+var BioModal = function(element) {
+    element = $(element);
+
+    this.setContestant = function(contestant, mode, clickHandler) {
+        // Remove old click handlers
+        element.contents().andSelf().off('click');
+
+        element.find('.close, .btn-large').andSelf().click(function() {
+            element.hide();
+        }).children().click(function(e) {
+                e.stopPropagation();
+        });
+
+        element.find('.title').text(contestant.name);
+
+        var stats = element.find('.stats').empty();
+        $.each(contestant.stats, function(i, v) {
+            stats.append('<div class="stat"><span class="name">' + v.key + ':</span> ' + v.value + '</div>');
+        });
+
+        var questions = element.find('.questions').empty();
+        $.each(contestant.questions, function(i, v) {
+            questions.append('<div class="question"><span class="q">' + v.key + '</span><br>' + v.value + '</div>');
+        });
+
+        element.find('.woman').backgroundImage(utils.getLargeImage(contestant));
+
+        element.find('.btn-large').hide();
+        switch (mode) {
+            case 'selectionClosed':
+                element.find('.selectionClosed').show();
+                break;
+            case 'selectionFull':
+                element.find('.selectionFull').show();
+                break;
+            case 'selectable':
+                element.find('.chooseMe').click(function() {
+                    clickHandler(true);
+                }).show();
+                break;
+            case 'rejectable':
+                element.find('.rejectMe').click(function() {
+                    clickHandler(false);
+                }).show();
+                break;
+        }
+        return this;
+    }
+
+    this.show = function() {
+        element.show();
+        return this;
+    }
+    this.hide = function() {
+        element.hide();
+        return this;
+    }
+}
+var bioModal = new BioModal('#bioModal');
