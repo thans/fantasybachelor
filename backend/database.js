8f89ac1f7c028d07465a343faa8e3a4d4565f90e
thans <tore@Tores-MacBook-Air.local>
2014-01-04 16:08:52 -0700

    Added getWeeks and getContestantData functionality
    Slow due to lots of database queries, some TODO in getWeeks (need
    actual predictions to see if it is working as well)

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 var orm = require("orm");
 var utils = require('./utils');
-
+var _und = require("./underscore-min");
 exports.orm = orm;
 
 exports.getExpressConnection = function() {
@@ -43,9 +43,206 @@ exports.getExpressConnection = function() {
                             callback(user);
                         });
                     }
-                })
+                });
+            };
+            models.week = db.define('week', {
+                name: 'text',
+                number: 'number',
+                openDatetime: 'date',
+                closeDatetime: 'date',
+                scoresAvailableDatetime: 'date'
+            }, {
+                methods: {
+                    isCurrent: function() {
+                        var today = new Date();
+                        return today > this.open_datetime && today < this.close_datetime;
+                    }
+                }
+            });
+            models.week.getWeeks = function(userz, callback) {
+                console.log('Getting week information');
+                var thiz = this;
+                var weekData = [];
+                var num;
+                var user = 1; //TODO need the user to get their predictions
+                this.find({}).each(function(item) {
+                    models.elimination.getEliminatedContestantsByWeek(item.id, function(eliminated) {
+                        models.contestant.getRemainingContestants(eliminated, function(remaining) {
+                            models.prediction.getSelectionByWeek(user, item.id, function(selections) {
+                                weekData.push({
+                                    id: item.id,
+                                    name: item.name,
+                                    openTime: item.openDatetime,
+                                    closeTime: item.closeDatetime,
+                                    scoresAvailableTime: item.scoresAvailableDatetime,
+                                    remainingContestants: remaining, 
+                                    selectedContestants: selections,
+                                    eliminatedContestants: eliminated, 
+                                    numberOfSelections: Math.min(remaining.length, 6) 
+                                });
+                                num--;
+                                if (num == 0) {
+                                    callback(weekData);
+                                }
+                            });
+                        });
+                        
+                    });
+
+                }).count(function(count) {
+                    num = count;
+                    console.log("final: " + count);
+                    
+                });          
             }
-        }
+
+
+            models.contestant = db.define('contestant', {
+                name: 'text',
+                codename: 'text'
+            });
+
+            models.contestant.getAllContestants = function(callback) {
+                var contestants = [];
+                this.find({}).each(function(oneContestant) {
+                    if (oneContestant.week_id <= weekId) {
+                        eliminations.push(oneContestant.contestant_id);
+                    }
+                }).count(function(count) {
+                    callback(contestants);
+                });
+            }
+
+            models.contestant.getContestantData = function(data, callback) {
+                var contestantData = [];
+                var num;
+                this.find({}).each(function(contestant) {
+                    models.bioStatistic.getStats(contestant.id, function(stats) {
+                        models.bioQuestion.getQuestions(contestant.id, function(questions) {
+                            contestantData.push({
+                                id: contestant.id,
+                                name: contestant.name,
+                                codeName: contestant.codeName,
+                                stats: stats,
+                                questions: questions
+                            });
+                            num--;
+                            if (num == 0) {
+                                callback(contestantData);
+                            }
+                        })
+                    })
+                }).count(function(count) {
+                    num = count;
+                });
+            }
+
+            models.contestant.getRemainingContestants = function(eliminated, callback) {
+                var allContestants = [];
+                this.find({}).each(function(one) {
+                    if (one.codename != "juanPablo") {
+                        allContestants.push(one.id);
+                    }
+                }).count(function(count) {
+                    callback(_und.difference(allContestants, eliminated));
+                });
+            }
+            models.elimination = db.define('elimination', {});
+            models.elimination.hasOne('contestant', models.contestant);
+            models.elimination.hasOne('week', models.week);
+            
+
+            models.elimination.getEliminatedContestantsByWeek = function(weekId, callback) {
+                var eliminations = [];
+                this.find({}).each(function(oneElimination) {
+                    if (oneElimination.week_id <= weekId) {
+                        eliminations.push(oneElimination.contestant_id);
+                    }
+                }).count(function(count) {
+                    callback(eliminations);
+                });
+            }
+ 
+
+            models.scoringOpportunity = db.define('scoringOpportunity', {
+                name: 'text',
+                type: ["NORMAL", "BONUS"],
+                value: 'number'
+            });
+            models.scoringOpportunity.hasOne('week', models.week);
+
+            /*models.scoringOpportunity.getWeeklyScoringOpportunities = function(opportinities, weekId, callback) {
+                var selections = [];
+                var scoringOpportunities = [];
+                this.find({}).each(function(opp) {
+                    if (_und.indexOf(opportunities, opp.id) != -1 && opp.week_id == weekId) {
+                        selections.push(opp.)
+                    }
+                    scoringOpportunities.push(selection.scoringOpportunity_id);
+                }).count(function(count) {
+                    console.log("eliminated " + eliminations);
+                    callback(eliminations);
+                });
+            }*/
+
+
+            models.prediction = db.define('prediction', {
+                createdDatetime: 'date',
+                user_id: 'number',
+            });
+            models.prediction.hasOne('contestant', models.contestant);
+            models.prediction.hasOne('scoringOpportunity', models.scoringOpportunity);
+
+            models.prediction.getSelectionByWeek = function(userId, weekId, callback) {
+                var selections = [];
+                // TODO filter by user and week
+                this.findByScoringOpportunity({}).each(function(selection) {
+                    scoringOpportunities.push(selection.scoringOpportunity_id);
+                }).count(function(count) {
+                    callback(selections);
+                });
+            }
+
+
+            models.bioQuestion = db.define('bioQuestion', {
+                question: 'text',
+                answer: 'text',
+            });
+            models.bioQuestion.hasOne('contestant', models.contestant);
+
+            models.bioQuestion.getQuestions = function(contestantId, callback) {
+                var bioSurvey = [];
+                // TODO filter by user and week
+                this.find({contestant_id : contestantId}).each(function(interview) {
+                    bioSurvey.push({
+                        key: interview.question,
+                        value: interview.answer
+                    });
+                }).count(function(count) {
+                    callback(bioSurvey);
+                });
+            }
+
+            models.bioStatistic = db.define('bioStatistic', {
+                name: 'text',
+                value: 'text',
+            });
+            models.bioStatistic.hasOne('contestant', models.contestant);
+
+            models.bioStatistic.getStats = function(contestantId, callback) {
+                var stats = [];
+                // TODO filter by user and week
+                this.find({contestant_id : contestantId}).each(function(stat) {
+                    stats.push({
+                        key: stat.name,
+                        value: stat.value
+                    });
+                }).count(function(count) {
+                    callback(stats);
+                });
+            }
+
+        },
     });
 }
 
