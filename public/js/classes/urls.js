6093cd055070ea2dac0ae3ea72134a3ec70cf5dc
Mitchell Loeppky <mitchell@loeppky.com>
2014-01-06 17:50:25 -0800

    Bug fixes

--- public/js/classes/urls.js
+++ public/js/classes/urls.js
@@ -1,5 +1,5 @@
 var URLS = {};
-URLS.BASE_URL = window.location.hostname === "localhost" ? 'http://localhost:8000' : 'http://server.fantasybach.com';
+URLS.BASE_URL = window.location.hostname === "localhost" ? 'http://localhost:8000' : '';
 URLS.GET_WEEKS = URLS.BASE_URL + '/getWeeks';
 URLS.GET_CONTESTANTS = URLS.BASE_URL + '/getContestants';
 URLS.LOGIN_USER = URLS.BASE_URL + '/loginUser';
