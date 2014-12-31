3e99af50c4dbc66102ee077d852d1b8c6d23024d
Mitchell Loeppky <mitchell@loeppky.com>
2014-12-30 20:45:10 -1000

    Removed files from previous version

--- src/public/js/classes/selectionModes.js
+++
@@ -1,7 +0,0 @@
-var SELECTION_MODES = {
-    CONTESTANT_UNSELECTABLE: 'Unselected', // This contestant can't be selected under any circumstance
-    SELECTION_CLOSED: 'Selection Closed', // Out of the time frame to select contestants
-    SELECTION_FULL: 'Selection Full', // All of the available selections are taken
-    REMOVABLE: 'Remove Me', // The contestant is selected and is therefore unselectable
-    CHOOSABLE: 'Choose Me' // THe contestant is unselected and is therefore selectable
-}
