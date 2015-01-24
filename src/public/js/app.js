6c0a901412aade908c421d514a69d951b08f6808
Mitchell Loeppky <mitchell@loeppky.com>
2015-01-23 20:55:53 -0800

    Added angular-animate dependency

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('FantasyBach', ['ngRoute', 'ngTouch']);
+var app = angular.module('FantasyBach', ['ngRoute', 'ngAnimate', 'ngTouch']);
 
 app.run(['$rootScope', '$q', 'userFactory', 'weeksFactory', 'contestantFactory', 'routeFactory', 'routeValidatorFactory', 'unauthorizedInterceptor', function($rootScope, $q, userFactory, weeksFactory, contestantFactory, routeFactory, routeValidatorFactory, unauthorizedInterceptor) {
     $q.all([userFactory.promise, userFactory.aliasPromise, contestantFactory.promise, weeksFactory.promise]).then(function(data) {
